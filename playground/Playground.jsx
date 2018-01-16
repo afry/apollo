eec095517b12a7aea81ed1519d131cf05f93000e
Jon Wahlström <jon.wahlstrom@afconsult.com>
2018-01-16 14:53:15 +0100

    Add skeleton

---
+++ playground/Playground.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Playground extends React.Component {
+    render() {
+        return (
+            <h1>Hello Playground!</h1>
+        );
+    }
+}
---
+++ playground/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Playground from './Playground';
+
+render(
+    <Playground />,
+    document.getElementById("root")
+);
