0a3fd1f1815508a50838a6f84982a1ab1e92c2ea
Jon Wahlström <jonwahlstrom@tarheadstudio.com>
2018-01-17 21:35:57 +0100

    Update Button component

--- src/components/Button/tests/Button.test.jsx
+++ src/components/Button/tests/Button.test.jsx
@@ -1,36 +1,58 @@
 import React from 'react';
-import Button from '../Button';
 import renderer from 'react-test-renderer';
+import Button from '../Button';
 
-test('onClick', () => {
-    const component = renderer.create(
-        <Button onClick={()=>{}} /> 
-    );
+describe('<Button />', () => {
+    describe('onClick()', () => {
+        it('is called when the button is clicked', () => {
+            const spy = jest.fn();
+            renderer.create(<Button onClick={spy} />).toJSON().props.onClick();
+            expect(spy).toHaveBeenCalled();
+        });
+    });
 
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    describe('onFocus()', () => {
+        it('is called when the button is focused', () => {
+            const spy = jest.fn();
+            renderer.create(<Button onFocus={spy} />).toJSON().props.onFocus();
+            expect(spy).toHaveBeenCalled();
+        });
+    });
 
-    tree.props.onClick();
-});
+    describe('onBlur()', () => {
+        it('is called when the button is blurred', () => {
+            const spy = jest.fn();
+            renderer.create(<Button onBlur={spy} />).toJSON().props.onBlur();
+            expect(spy).toHaveBeenCalled();
+        });
+    });
 
-test('onMouseEnter', () => {
-    const component = renderer.create(
-        <Button onMouseEnter={()=>{}} /> 
-    );
+    describe('disabled', () => {
+        it('sets the disabled attribute on the button', () => {
+            const button = renderer.create(<Button disabled />);
+            expect(button.toJSON().props.disabled).toBe(true);
+        });
 
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+        it('sets the disabled attribute on the button', () => {
+            const button = renderer.create(<Button disabled={false} />);
+            expect(button.toJSON().props.disabled).toBe(false);
+        });
+    });
 
-    tree.props.onMouseEnter();
-});
+    describe('type', () => {
+        it('sets the type attribute on the button to default', () => {
+            const button = renderer.create(<Button />);
+            expect(button.toJSON().props.type).toBe('button');
+        });
 
-test('onMouseLeave', () => {
-    const component = renderer.create(
-        <Button onMouseLeave={()=>{}} /> 
-    );
+        it('sets the type attribute on the button to submit', () => {
+            const button = renderer.create(<Button type='submit' />);
+            expect(button.toJSON().props.type).toBe('submit');
+        });
 
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
-    tree.props.onMouseLeave();
+        it('sets the type attribute on the button to reset', () => {
+            const button = renderer.create(<Button type='reset' />);
+            expect(button.toJSON().props.type).toBe('reset');
+        });
+    });
 });
