1d3de44dca034a6e9a8fbb386b5855fc246a3ad3
Jon Wahlström <jonwahlstrom@tarheadstudio.com>
2018-01-17 21:55:31 +0100

    Update tests

--- src/components/Button/tests/Button.test.jsx
+++ src/components/Button/tests/Button.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 import Button from '../Button';
 
 describe('<Button />', () => {
     describe('onClick()', () => {
         it('is called when the button is clicked', () => {
             const spy = jest.fn();
-            renderer.create(<Button onClick={spy} />).toJSON().props.onClick();
+            shallow(<Button onClick={spy} />).simulate('click');
             expect(spy).toHaveBeenCalled();
         });
     });
@@ -14,7 +14,7 @@ describe('<Button />', () => {
     describe('onFocus()', () => {
         it('is called when the button is focused', () => {
             const spy = jest.fn();
-            renderer.create(<Button onFocus={spy} />).toJSON().props.onFocus();
+            shallow(<Button onFocus={spy} />).simulate('focus');
             expect(spy).toHaveBeenCalled();
         });
     });
@@ -22,37 +22,41 @@ describe('<Button />', () => {
     describe('onBlur()', () => {
         it('is called when the button is blurred', () => {
             const spy = jest.fn();
-            renderer.create(<Button onBlur={spy} />).toJSON().props.onBlur();
+            shallow(<Button onBlur={spy} />).simulate('blur');
             expect(spy).toHaveBeenCalled();
         });
     });
 
     describe('disabled', () => {
         it('sets the disabled attribute on the button', () => {
-            const button = renderer.create(<Button disabled />);
-            expect(button.toJSON().props.disabled).toBe(true);
+            const button = shallow(<Button disabled />);
+            expect(button.prop('disabled')).toBe(true);
         });
 
         it('sets the disabled attribute on the button', () => {
-            const button = renderer.create(<Button disabled={false} />);
-            expect(button.toJSON().props.disabled).toBe(false);
+            const button = shallow(<Button disabled={false} />);
+            expect(button.prop('disabled')).toBe(false);
         });
     });
 
     describe('type', () => {
         it('sets the type attribute on the button to default', () => {
-            const button = renderer.create(<Button />);
-            expect(button.toJSON().props.type).toBe('button');
+            const button = shallow(<Button />);
+            expect(button.prop('type')).toBe('button');
         });
 
+        it('sets the type attribute on the button to button', () => {
+            const button = shallow(<Button type="button"/>);
+            expect(button.prop('type')).toBe('button');
+        });
         it('sets the type attribute on the button to submit', () => {
-            const button = renderer.create(<Button type='submit' />);
-            expect(button.toJSON().props.type).toBe('submit');
+            const button = shallow(<Button type="submit" />);
+            expect(button.prop('type')).toBe('submit');
         });
 
         it('sets the type attribute on the button to reset', () => {
-            const button = renderer.create(<Button type='reset' />);
-            expect(button.toJSON().props.type).toBe('reset');
+            const button = shallow(<Button type="reset" />);
+            expect(button.prop('type')).toBe('reset');
         });
     });
 });
