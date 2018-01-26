3566d30edd345e2a00be66defaeaa69714af70a3
Jon Wahlström <jon.wahlstrom@afconsult.com>
2018-01-26 15:10:14 +0100

    Add travis

--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -3,36 +3,21 @@ const webpack = require('webpack');
 const combineLoaders = require('webpack-combine-loaders');
 
 module.exports = {
+  devServer: {
+    contentBase: path.resolve(__dirname),
+  },
   entry: [
     'react-hot-loader/patch',
     path.join(__dirname, 'index.jsx'),
   ],
-  output: {
-    filename: 'apollo.js',
-    path: path.resolve(__dirname, 'assets'),
-    publicPath: '/assets/',
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname),
-  },
-  resolve: {
-    extensions: ['.jsx', '.js', '.json'],
-    alias: {
-      '@afconsult/apollo': path.resolve(__dirname, '../src'),
-    },
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
   module: {
     loaders: [
       {
-        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: ['babel-loader'],
+        test: /\.(js|jsx)$/,
       },
       {
-        test: /\.css$/,
         loader: combineLoaders([
           {
             loader: 'style-loader',
@@ -40,12 +25,27 @@ module.exports = {
           {
             loader: 'css-loader',
             query: {
-              modules: true,
               localIdentName: '[name]__[local]__[hash:base64:5]',
+              modules: true,
             },
           },
         ]),
+        test: /\.css$/,
       },
     ],
+  },
+  output: {
+    filename: 'apollo.js',
+    path: path.resolve(__dirname, 'assets'),
+    publicPath: '/assets/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@afconsult/apollo': path.resolve(__dirname, '../src'),
+    },
+    extensions: ['.jsx', '.js', '.json'],
   },
 };
