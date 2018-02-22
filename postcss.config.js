9cd405e2170a380392988da90dbe3249878dd5c6
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-02-22 08:47:17 +0100

    Add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
+const autoprefixer = require('autoprefixer'); // eslint-disable-line import/no-extraneous-dependencies
+
 module.exports = {
   plugins: {
+    autoprefixer: autoprefixer({ browsers: ['last 2 versions', '> 5%', 'iOS >= 8'] }),
     'postcss-import': {},
     'postcss-cssnext': { // eslint-disable-line sort-keys
       browsers: ['last 2 versions', '> 5%']
