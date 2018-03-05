ebcee79117d973f0b5326603aa497f3ab40e960e
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-03-05 16:19:32 +0100

    Update Wizard components
    - Now pass title and description as html elements to WizardPages
    - WizardPage title is now used only for navigation display
    - Missing validation
    - Missing complete navbar implementation

--- src/components/WizardPage/WizardPage.jsx
+++ src/components/WizardPage/WizardPage.jsx
@@ -1,41 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import Header from '../Heading';
-
 import * as styles from './WizardPage.css';
 
 const propTypes = {
-  children: PropTypes.node,
-  description: PropTypes.string,
   title: PropTypes.string,
 };
 
 const defaultProps = {
-  children: undefined,
-  description: undefined,
   title: undefined,
 };
 
 const WizardPage = (props) => {
   const {
-    children,
-    description,
-    title,
+    ...other
   } = props;
 
   return (
-    <div className={styles['wizard-page']}>
-      <div className={styles['wizard-page-title']}>
-        <Header h1>{title}</Header><br />
-        <p className={styles['wizard-page-description']}>
-          {description}
-        </p><br />
-      </div>
-      <div>
-        {children}
-      </div>
-    </div>
+    <div {...other} className={styles['wizard-page']} />
   );
 };
 
