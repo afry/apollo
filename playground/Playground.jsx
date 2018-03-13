3e6d15c6dac1d0d0cd1c028c316439ab6eadce77
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-03-13 15:59:18 +0100

    Update Playground

--- playground/Playground.jsx
+++ playground/Playground.jsx
@@ -2,11 +2,16 @@
 import React from 'react';
 
 import NavsExample from './examples/NavsExample';
-import FormExample from './examples/FormExample';
+import NavbarExample from './examples/NavbarExample';
+
+
+import Avatar from '../src/components/Avatar';
 
 export default function Playground() {
   return (
     <div>
+      <Avatar alt="JW" size="x-small" src="404" />
+      <NavbarExample />
       <NavsExample /> 
     </div>
   );
