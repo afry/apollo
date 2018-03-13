49bcd1be2d9fec2f9592c647c337c7d9be82771a
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-03-13 15:57:24 +0100

    Add Navbar component with example

---
+++ playground/examples/NavbarExample.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from '../../src';
+
+const NavbarExample = () => {
+
+  return (
+    <div className="example">
+      <h1>Navbar</h1>
+
+      <h3>Dark</h3>
+      <Navbar>
+        <NavbarBrand>one</NavbarBrand>
+        <Nav navbar>
+          <NavItem>
+            <NavLink>Time Report</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Projects</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Groups</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Information</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+
+      <h3>Light</h3>
+      <Navbar inverse>
+        <NavbarBrand>Home</NavbarBrand>
+        <Nav horizontal="center" navbar>
+          <NavItem>
+            <NavLink active>Overview</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>News</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>CEO Blog</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Jobs & Assignments</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarExample;
