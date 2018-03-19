9c2d01733af9af4b7b94ce21deaa046c8128acbf
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-03-19 14:56:43 +0100

    Add Navlink tests

---
+++ src/components/NavLink/tests/NavLink.test.jsx
@@ -0,0 +1,26 @@
+/* global describe, it, expect, jest */
+import React from 'react';
+import { shallow } from 'enzyme';
+import NavLink from '../NavLink';
+
+describe('<NavLink />', () => {
+  describe('onClick()', () => {
+    it('is called when the navlink is clicked', () => {
+      const spy = jest.fn();
+      shallow(<NavLink onClick={spy} />).simulate('click');
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('disabled', () => {
+    it('sets the disabled attribute on the navlink', () => {
+      const button = shallow(<NavLink disabled />);
+      expect(button.prop('disabled')).toBe(true);
+    });
+
+    it('sets the disabled attribute on the navlink', () => {
+      const button = shallow(<NavLink disabled={false} />);
+      expect(button.prop('disabled')).toBe(false);
+    });
+  });
+});
