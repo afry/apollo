1f5147939db5101b4f1d2a94072783c2fa51b01c
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-04-06 13:56:43 +0200

    Update wizard tests

--- src/components/Wizard/tests/Wizard.test.jsx
+++ src/components/Wizard/tests/Wizard.test.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Wizard from '../Wizard';
-import WizardPage from '../../WizardPage';
+import WizardStep from '../../WizardStep';
 
 describe('<Wizard />', () => {
   describe('onNext', () => {
     it('is called when the wizard move on to the next page', () => {
       const spy = jest.fn();
-      const wizard = mount(<Wizard onNext={spy} title="My Wizard"><WizardPage title="Page1" /><WizardPage title="Page2" /></Wizard>);
+      const wizard = mount(<Wizard onNext={spy} title="My Wizard"><WizardStep label="Page1" /><WizardStep label="Page2" /></Wizard>);
       wizard.prop('onNext')(spy);
       expect(spy).toHaveBeenCalled();
     });
@@ -17,7 +17,7 @@ describe('<Wizard />', () => {
   describe('onPrevious', () => {
     it('is called when the wizard goes back the previous page', () => {
       const spy = jest.fn();
-      const wizard = mount(<Wizard onPrevious={spy} title="My Wizard"><WizardPage title="Page1" /><WizardPage title="Page2" /></Wizard>);
+      const wizard = mount(<Wizard onPrevious={spy} title="My Wizard"><WizardStep label="Page1" /><WizardStep label="Page2" /></Wizard>);
       wizard.prop('onPrevious')(spy);
       expect(spy).toHaveBeenCalled();
     });
@@ -26,7 +26,7 @@ describe('<Wizard />', () => {
   describe('onCancel', () => {
     it('is called when the wizard is cancelled', () => {
       const spy = jest.fn();
-      const wizard = mount(<Wizard onCancel={spy} title="My Wizard"><WizardPage title="Page1" /><WizardPage title="Page2" /></Wizard>);
+      const wizard = mount(<Wizard onCancel={spy} title="My Wizard"><WizardStep label="Page1" /><WizardStep label="Page2" /></Wizard>);
       wizard.prop('onCancel')(spy);
       expect(spy).toHaveBeenCalled();
     });
@@ -35,7 +35,7 @@ describe('<Wizard />', () => {
   describe('onFinish', () => {
     it('is called when the wizard is finished', () => {
       const spy = jest.fn();
-      const wizard = mount(<Wizard onFinish={spy} title="My Wizard"><WizardPage title="Page1" /><WizardPage title="Page2" /></Wizard>);
+      const wizard = mount(<Wizard onFinish={spy} title="My Wizard"><WizardStep label="Page1" /><WizardStep label="Page2" /></Wizard>);
       wizard.prop('onFinish')(spy);
       expect(spy).toHaveBeenCalled();
     });
