1cca7e4b9140ede81af3be85d5ec6346be6acd5c
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-05-17 12:41:16 +0200

    Add className prop to Tag

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -5,11 +5,13 @@ import * as styles from './Tag.css';
 
 const propTypes = {
   children: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string,
   onClose: PropTypes.func,
 };
 
 const defaultProps = {
   children: undefined,
+  className: undefined,
   onClose: undefined,
 };
 
@@ -28,30 +30,35 @@ class Tag extends React.Component {
   }
 
   render() {
-    const { children, onClose } = this.props;
-    const outerClasses = classNames(styles.tag);
-    const textClasses = classNames(styles.text);
-    const closeClasses = classNames(styles.close);
+    const {
+      children,
+      className,
+      onClose
+    } = this.props;
 
-    if (!onClose) {
-      return (
-        <span className={outerClasses}>
-          <span className={textClasses}>{children}</span>
-        </span>
+    const classes = classNames(
+      className,
+      styles.tag
+    );
+
+    let closeButton = null;
+    if (onClose) {
+      closeButton = (
+        <button
+          area-label="Close"
+          className={classNames(styles.close)}
+          onClick={this.handleClose}
+          type="button"
+        />
       );
     }
 
     return (
-      <span className={outerClasses}>
-        <span className={textClasses}>
+      <span className={classes}>
+        <span className={classNames(styles.text)}>
           {children}
         </span>
-        <button
-          area-label="Close"
-          className={closeClasses}
-          onClick={this.handleClose}
-          type="button"
-        />
+        {closeButton}
       </span>
     );
   }
