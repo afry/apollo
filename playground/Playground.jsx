f7dc0a4361b820d87d52088e302fb7f1e5bdcb50
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-06-18 20:25:12 +0200

    Add default playground

--- playground/Playground.jsx
+++ playground/Playground.jsx
@@ -1,35 +1,12 @@
 /* eslint-disable */
 import React from 'react';
 
-import CardExample from './examples/CardExample';
-import IconExample from './examples/IconExample';
-import AvatarExample from './examples/AvatarExample';
-import BadgeExample from './examples/BadgeExample';
 import ButtonExample from './examples/ButtonExample';
-import DropdownExample from './examples/DropdownExample';
-import JumbotronExample from './examples/JumbotronExample';
-import NavsExample from './examples/NavsExample';
-import NavbarExample from './examples/NavbarExample';
-import FormExample from './examples/FormExample';
-import TagExample from './examples/TagExample';
-import MentionExample from './examples/MentionExample';
-import ModalExample from './examples/ModalExample';
 
 export default function Playground() {
   return (
     <div>
-      <ModalExample />
-      <TagExample />
-      {/* <MentionExample /> */}
-      {/* <IconExample /> */}
-      {/* <AvatarExample /> */}
-      {/* <BadgeExample /> */}
       <ButtonExample />
-      {/* <DropdownExample /> */}
-      {/* <JumbotronExample /> */}
-      {/* <NavbarExample /> */}
-      <NavsExample />
-      {/* <FormExample />  */}
     </div>
   );
 }
