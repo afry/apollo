9db09de0c53416228d851b2467c5442461ac04e9
Wahlström Jon <jon.wahlstrom@afconsult.com>
2018-06-20 10:04:01 +0200

    Add dark theme

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,17 +5,20 @@ import * as styles from './Card.css';
 
 const propTypes = {
   className: PropTypes.string,
+  dark: PropTypes.bool,
   tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
 
 const defaultProps = {
   className: undefined,
+  dark: false,
   tag: 'div',
 };
 
 const Card = (props) => {
   const {
     className,
+    dark,
     tag: Tag,
     ...other
   } = props;
@@ -23,6 +26,7 @@ const Card = (props) => {
   const classes = classNames(
     className,
     styles.card,
+    dark ? styles['card-dark'] : '',
   );
 
   return (
