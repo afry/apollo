0dccb2c6e52f46fa81ad07bfddf9d6ffb86113dd
Jon Wahlström <ejfwahlstrom@gmail.com>
2018-12-10 09:21:25 +0100

    Add alert colors (#28)

--- playground/Playground.jsx
+++ playground/Playground.jsx
@@ -18,13 +18,39 @@ import {
 } from './examples';
 
 import {
-  Alert
+  Alert,
+  Container
 } from '../src';
 
-export default function Playground() {
-  return (
-    <div>
-      <Alert />
-    </div>
-  );
+class Playground extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: true };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle(e) {
+    console.log('before', this.state)
+    this.setState({ open: false }, () => { console.log('after', this.state)});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <Alert />
+          <Alert color="secondary" />
+          <Alert open={this.state.open} color="danger" onToggle={this.handleToggle} />
+          <Alert color="success" />
+          <Alert color="warning" closeAfter={1000*5}>
+            <b>ERROR</b> This is an example of the alert component!
+          </Alert>
+          <Alert color="info" />
+        </Container>
+        <FormExample />
+      </React.Fragment>
+    );
+  }
 }
+
+export default Playground;
